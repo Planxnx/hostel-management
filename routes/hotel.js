1d766d75b3451c0a795318279a7d2111e9de21cb
Planxnx <60010465@kmitl.ac.th>
2019-12-18 04:37:56 +0700

    add hotel not found handle

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const JWTAuthMiddleware = require('../middleware/jwtAuth')
 const hotelService = require('../database/hotel/hotelService')
 
 router.get('/', (req, res, next) => {
@@ -11,10 +11,19 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/info/:id', (req, res, next) => {
-  res.json({
-    status: 200,
-    data: hotelService.getHotel(req.params.id)
-  });
+  let hotelInfo = hotelService.getHotel(req.params.id)
+  if (hotelInfo){
+    res.json({
+      status: 200,
+      data: hotelInfo
+    });
+  } else {
+    res.json({
+      status: 404,
+      message: "hotel not found"
+    });
+  }
+  
 });
 
 router.get('/available', (req, res, next) => {
