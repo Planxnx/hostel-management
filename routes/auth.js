85f349047b5c2492f86682769803cb819b51ae25
Planxnx <60010465@kmitl.ac.th>
2019-12-18 03:10:16 +0700

    add login path

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,36 @@ router.post('/register', (req, res, next) => {
       message: createStatus
     });
   }
+});
 
+router.post('/login', (req, res, next) => {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.json({
+      status: 400,
+      message: "not found data"
+    });
+    return
+  }
+
+  let userPayload = userService.authenticate({
+    username: req.body.username,
+    password: req.body.password
+  })
+  
+  if (!userPayload) {
+    res.json({
+      status: 401,
+      message: "wrong username or password"
+    });
+  } else {
+    res.json({
+      status: 200,
+      data: {
+        token: userPayload.token,
+        username: userPayload.username
+      }
+    });
+  }
 });
 
 module.exports = router;
