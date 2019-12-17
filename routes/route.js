a139c76d97626d74bc325848a80b6b646a46c920
Planxnx <60010465@kmitl.ac.th>
2019-12-18 01:50:02 +0700

    add auth route

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const hotelRoute = require('./hotel');
+const authRoute = require('./auth');
 
 router.use('/hotel', hotelRoute);
+router.use('/auth', authRoute);
 
 router.get('/healthcheck', (req, res, next) => {
   res.json({
